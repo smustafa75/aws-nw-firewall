84c82467e218815e7cc7daed9401c5e6f495c136
صابر مصطفی <sabrmstfy@Mac.lan>
2024-12-15 21:10:15 +0400

    outputs updated

--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,11 @@ output "Public_Subnets" {
   description = "Public Subnets"
   value       = concat(module.network.public_net, module.network.public_subnets_cidr)
 }
+
+output "private_instance" {
+  value = module.compute.private_instance
+}
+
 /*
 output "Bucket_ID" {
   value = module.storage.bucketname
@@ -30,9 +35,7 @@ output "SQL_Bucket_ARN" {
 }
 
 
-output "hana_db01" {
-  value = module.compute.hana_db01_id
-}
+
 */
 /*
 output "public_ip" {
